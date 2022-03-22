045ee16ae8a5e250c9f0c20d8585fc1a8ae833f8
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-22 03:47:38 -0300

    Modified shape attributes

--- Graphics-Engine/Graphics-Engine/src/Shape.cpp
+++ Graphics-Engine/Graphics-Engine/src/Shape.cpp
@@ -6,28 +6,28 @@ Shape::Shape(Renderer& renderer) : Entity2D(renderer) {
 
 void Shape::ChangeColor(float r, float g, float b) {
 	vertex[3] = r;
-	vertex[10] = r;
-	vertex[17] = r;
-	vertex[24] = r;
+	vertex[12] = r;
+	vertex[21] = r;
+	vertex[30] = r;
 
 	vertex[4] = g;
-	vertex[11] = g;
-	vertex[18] = g;
-	vertex[25] = g;
+	vertex[13] = g;
+	vertex[22] = g;
+	vertex[31] = g;
 
 	vertex[5] = b;
-	vertex[12] = b;
-	vertex[19] = b;
-	vertex[26] = b;
+	vertex[14] = b;
+	vertex[23] = b;
+	vertex[32] = b;
 }
 
 void Shape::ChangeAlpha(float alpha) {
 	vertex[6] = alpha;
-	vertex[13] = alpha;
-	vertex[20] = alpha;
-	vertex[27] = alpha;
+	vertex[15] = alpha;
+	vertex[24] = alpha;
+	vertex[33] = alpha;
 }
 
 void Shape::Draw() {
-	render->Draw(vertex, index, transform.Model(), 0);
+	render->Draw(vertex, index, transform.Model());
 }
