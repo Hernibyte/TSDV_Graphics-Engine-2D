201e160b4bc9d46b2c2684f7c45bb455e18d60a9
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-23 08:37:24 -0300

    Added view & projection matrix

--- Graphics-Engine/Graphics-Engine/src/StandardShader/StandardShader.glsl
+++ Graphics-Engine/Graphics-Engine/src/StandardShader/StandardShader.glsl
@@ -9,9 +9,11 @@ out vec4 ourColor;
 out vec2 texCoord;
 
 uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-	gl_Position = model * vec4(inPosition, 1.0);
+	gl_Position = projection * view * model * vec4(inPosition, 1.0);
 	ourColor = inColor;
 	texCoord = inTexCoord;
 }
